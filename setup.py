3c420655531fca6929b66a1c942a434fb7e7ba13
Simon Law <SimonL@trustcentric.com>
2011-03-04 16:57:45 -0500

    Version 1.7.

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from setuptools import setup
 
 setup(
     name='python-digest',
-    version='1.6',
+    version='1.7',
     description=('A Python library to aid in implementing HTTP Digest Authentication.'),
     long_description=(
 """
