b4c2e3a274bd172654095b702cd3a52bcee08ad5
Nick Pellegrino <npellegrino@dimagi.com>
2018-03-14 10:14:29 -0400

    fix issue when setting password with international characters

--- python_digest/__init__.py
+++ python_digest/__init__.py
@@ -45,7 +45,7 @@ def calculate_partial_digest(username, realm, password):
     Calculate a partial digest that may be stored and used to authenticate future
     HTTP Digest sessions.
     '''
-    return md5.md5("%s:%s:%s" % (username.encode('utf-8'), realm, password.encode('utf-8'))).hexdigest()
+    return md5.md5(b"%s:%s:%s" % (username.encode('utf-8'), realm, password.encode('utf-8'))).hexdigest()
 
 def build_digest_challenge(timestamp, secret, realm, opaque, stale):
     '''
